5024d1ed8a2d8c93a1e11730691067461a06844f
Alex Campbell <campbellar2@vcu.edu>
2022-03-31 19:01:57 -0400

    added very basic ui to site

--- script.js
+++ script.js
@@ -1,59 +1,74 @@
+//grabbing buttons from index
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+const score = document.querySelector('p')
+const results = document.querySelector('h4')
+
+let playerScore = 0;
+let computerScore = 0;
+
+//script to play a round on click of buttons
+rock.addEventListener('click', () => playRound("rock", computerPlay()));
+paper.addEventListener('click', () => playRound("paper", computerPlay()));
+scissors.addEventListener('click', () => playRound("scissors", computerPlay()));
+
+
 // A function to create the computer 'opponent' user will be playing against 
 function computerPlay() {
-    const choices = ["Rock", "Paper", "Scissors"];
+    const choices = ["rock", "paper", "scissors"];
     return choices[Math.floor(Math.random()*choices.length)]
 }
 // A function to play a round of RPS
 function playRound(playerSelection, computerSelection) {
+    if (playerScore === 5 || computerScore === 5) {
+        score.textContent = "You've completed the game! Please reload page to play again."
+        return
+    }
     if (playerSelection === "rock") {
-        if (computerSelection === "Rock") { return "Tie" }
-        else if (computerSelection === "Paper") { return "Lose" }
-        else { return "Win" }
-    } else if (playerSelection === "paper") {
-        if (computerSelection === "Rock") { return "Win" }
-        else if (computerSelection === "Paper") { return "Tie" }
-        else { return "Lose" }
-    } else if (playerSelection === "scissors") {
-        if (computerSelection === "Rock") { return "Lose" }
-        else if (computerSelection === "Paper") { return "Win" }
-        else { return "Tie" }
-    } else { return "Error" }
-}
-
-// function for the full game
-function game() {
-    //player score
-    let p = 0
-    //computer score
-    let c = 0
-    for (let i = 0; i < 5; i++) {
-        // playerSelection initialization
-        let playerSelection = window.prompt("Rock, Paper, or Scissors").toLowerCase();
-        // computerSelection initialization
-        let computerSelection = computerPlay();
-        // actual game start
-        if (playRound(playerSelection, computerSelection) === "Tie") {
-            // on tie, decrements counter
-            console.log("Your score: " + p.toString() + "\nThe Computer: " + c.toString())
-            i--
-        } else if (playRound(playerSelection, computerSelection) === "Lose") {
-            // on lose, increments computer score
-            c++
-            console.log("Your score: " + p.toString() + "\nThe Computer: " + c.toString())
-        } else if (playRound(playerSelection, computerSelection) === "Win") {
-            //on win, increments player score
-            p++
-            console.log("Your score: " + p.toString() + "\nThe Computer: " + c.toString())
-        } else {
-            //error or typo in player selection catch
-            i--
-            console.log("Error in your selection, please try again")
+        if (computerSelection === "rock") { 
+            results.textContent = "Tie!"
         }
+        else if (computerSelection === "paper") { 
+            results.textContent = "Lose!"
+            computerScore++
+        }
+        else { 
+            results.textContent = "Win!"
+            playerScore++
+        }
+    } else if (playerSelection === "paper") {
+        if (computerSelection === "rock") { 
+            results.textContent = "Win!"
+            playerScore++
+        }
+        else if (computerSelection === "paper") { 
+            results.textContent = "Tie!" 
+        }
+        else { 
+            results.textContent = "Lose!"
+            computerScore++
+        }
+    } else if (playerSelection === "scissors") {
+        if (computerSelection === "rock") { 
+            results.textContent = "Lose!" 
+            computerScore++
+        }
+        else if (computerSelection === "paper") { 
+            results.textContent = "Win!" 
+            playerScore++
+        }
+        else { 
+            results.textContent = "Tie!"
+        }
+    } else { 
+        return "Error" 
     }
-    //results
-    if (p > c) {
-        console.log("Congratulations! You win!")
-    } else {
-        console.log("Apologies, the computer won :(")
-    }
+    score.textContent = `Score: ${playerScore}-${computerScore}`
+    if (playerScore === 5) {
+        results.textContent += " Congratulations! You beat the computer!"
+    } 
+    if (computerScore === 5) {
+        results.textContent += " Apologies! The computer is smarter than you :("
+    } 
 }
